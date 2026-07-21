f78d36cf907b57688ab81ae4cd19948e0a83921b
Amercii <amerciiiii@gmail.com>
2026-07-21 21:37:28 +0100

    Ajout de Streamlit

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(
+    page_title="Simulation de mécanique",
+    layout="wide"
+)
+
+st.title("Simulation de mécanique")
+
+components.iframe(
+    "http://localhost:5173",
+    height=900,
+    scrolling=True
+)
